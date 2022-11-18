ec51b19847a803a859e14f111c87513826308d00
Thibaut Montanier <thibaut.montanier@gmail.com>
2022-11-18 16:38:23 +0100

    Exo 1 complet

--- CSharpIntro/Program.cs
+++ CSharpIntro/Program.cs
@@ -1,52 +1,56 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.Reflection.Metadata.Ecma335;
+﻿internal class Program {
 
-internal class Program {
     private static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
 
+        //// demander le nom à l'utilisateur
 
-        // demander le nom à l'utilisateur
-        Console.WriteLine("Quel est ton nom ?");
-        string nom;
-        nom = Console.ReadLine();
+        string nom = DemanderString("Quel est ton nom ?");
+        int age = DemanderNumeric("Quel est ton age ?");
+        int taille = DemanderNumeric("Quel est ta taille ?");
 
-        // demander l'age à l'utilisateur
+        
+        AfficherMessageResultat(nom, age, taille);
+
+    }
+
+    static string DemanderString(string question) {
+        Console.WriteLine(question);
+        return Console.ReadLine();
+    }
+    static int DemanderNumeric(string question) {
         int ageNumeric = 0;
-        bool lUtilisateurASaisiUnNumeric = false; // l'utilisateur n'a pas encore réalisé de saisie valide
+        bool lUtilisateurASaisiUnNumericPositif = false; // l'utilisateur n'a pas encore réalisé de saisie valide
 
         // tant que l'utilisateur n'a pas réalisé de saisie valide, on lui demande de saisir un numérique
-        while (lUtilisateurASaisiUnNumeric == false) {
+        while (lUtilisateurASaisiUnNumericPositif == false) {
             // demande de saisie puis lecture
-            Console.WriteLine("Quel est ton age ?");
+            Console.WriteLine(question);
             string ageSaisi = Console.ReadLine();
 
             // tentative de conversion de texte vers du numérique
             bool ageSaisiEstUnNumeric = int.TryParse(ageSaisi, out ageNumeric);
             // si la tentative est un succès => l'utilisateur a fait une saisie valide
             if (ageSaisiEstUnNumeric) {
-                lUtilisateurASaisiUnNumeric = true;
+                lUtilisateurASaisiUnNumericPositif = ageNumeric >= 0;
             } else {
                 // si la tentative de conversion est un échec, on précise que l'utilisateur n'a pas saisi de numérique 
                 Console.WriteLine("L'utilisateur n'a pas saisi un numérique");
             }
         }
-
-        // construction du message de résultat puis affichage de celui-ci
-        Program.AfficherMessageResultat(nom, ageNumeric.ToString());
-
+        return ageNumeric;
     }
 
 
-    static void AfficherMessageResultat(string nom, string age) {
+    static void AfficherMessageResultat(string nom, int age, int taille) {
         string messageResultat;
-        if (age == "1") {
+        if (age == 1) {
             messageResultat = "Bonjour, tu t'appelles " + nom + ", tu as " + age + " an";
-        } else if (age == "1") {
+        } else if (age == 0) {
             messageResultat = "Bonjour, tu es un tout jeune bébé";
         } else {
             messageResultat = "Bonjour, tu t'appelles " + nom + ", tu as " + age + " ans";
         }
         Console.WriteLine(messageResultat);
     }
+
 }
